194c89a2f7eac835599c9829b3d6f828e2051733
sommerfeld <sommerfeld@sommerfeld.dev>
2024-04-24 15:48:45 +0100

    Add script to create and sign release manifests

---
+++ utils/create-signed-manifest.sh
@@ -0,0 +1,26 @@
+#|/usr/bin/env sh
+
+set -e
+
+script_dir=$(dirname -- "$(readlink -f "$0")")
+
+tag=$(git -C "$script_dir" describe --tags --abbrev=0)
+
+cwd=$(pwd)
+cd "$(mktemp -d)"
+
+wget "https://github.com/sommerfelddev/sentrum/releases/download/$tag/sentrum-$tag-darwin-x86_64.tar.gz"
+wget "https://github.com/sommerfelddev/sentrum/releases/download/$tag/sentrum-$tag-linux-x86_64.tar.gz"
+wget "https://github.com/sommerfelddev/sentrum/releases/download/$tag/sentrum-$tag-windows-x86_64.zip"
+
+sha256sum -b -- * > sentrum-"$tag"-manifest.txt
+
+sha256sum --check sentrum-"$tag"-manifest.txt
+
+gpg -b --armor sentrum-"$tag"-manifest.txt
+
+gpg --verify sentrum-"$tag"-manifest.txt.asc
+
+pwd
+
+cd "$cwd"
